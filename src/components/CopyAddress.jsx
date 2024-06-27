4bd1aa555b92e4eb891bb021d54e95cb81ffae94
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-06-27 13:46:05 +0530

    Update CopyAddress.jsx

--- src/components/CopyAddress.jsx
+++ src/components/CopyAddress.jsx
@@ -3,7 +3,7 @@ import { Tooltip } from 'react-tooltip'
 
 const CopyAddress = () => {
   const [tooltipText, setTooltipText] = useState('Copy this');
-  const address = '5NHd3MsP6dXi9r1saPkeB2DoZyXLvPiqv4n9J54Cpump';
+  const address = 'HTRkZF9SzcZQpVrDueRebQeApXiYrqdHciYtRah8pVcb';
 
   const handleCopy = () => {
     navigator.clipboard.writeText(address).then(() => {
